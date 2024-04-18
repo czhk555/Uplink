75426689a6384e6592d738fc4869fd4b922ede50
Flemmli97 <34157027+Flemmli97@users.noreply.github.com>
2024-04-18 19:05:39 +0200

    fix(ui): rfd import for file dialog fix (#1978)
    Co-authored-by: Flemmli97 <Flemmli97@users.noreply.github.com>
    Co-authored-by: Luis E <35935591+luisecm@users.noreply.github.com>

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -1,4 +1,7 @@
 #![cfg_attr(feature = "production_mode", windows_subsystem = "windows")]
+// Having it here fixes a file dialog crash for some reason. (Seems to only be on some MacOS)
+#[allow(unused_imports)]
+use rfd::FileDialog;
 
 fn main() {
     uplink::main_lib();
